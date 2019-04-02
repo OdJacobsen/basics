a585f673a6c118acd578640c00c0975aef9533a3
OdJacobsen <od.p.jacobsen@gmail.com>
2019-04-02 14:18:12 +0200

    - Changed import to Choice from random - Changed random.choices(value, k=n) to multi-line choice - Added while loops to check if word is same - Added return to function rather than print

--- poem.py
+++ poem.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 
 # Work in progress.
 # Add While loop to add 3 different words from each list
@@ -14,11 +14,57 @@ adverbs = ['curiously', 'extravagantly', 'tantalizingly', 'furiously', 'sensuous
 
 
 def make_poem():
-    n = random.choice(nouns)
-    v = random.choice(verbs)
-    adj = random.choice(adjectives)
-    adv = random.choice(adverbs)
-    pre = random.choice(prepositions)
+    """Create a randomly generated poem, returned as a multi-line string.
+    While loops keep looping until first noun is not second noun, as well for noun 3.
+    """
+    # Pull three nouns randomly
+    n1 = choice(nouns)
+    n2 = choice(nouns)
+    n3 = choice(nouns)
+    #Make sure that all are different 
+    while n1 == n2:
+        n2 = choice(nouns)
+    while n1 == n3 or n2 == n3:
+        n3 = choice(nouns)
+    
+    # Pull three random verbs
+    v1 = choice(verbs)
+    v2 = choice(verbs)
+    v3 = choice(verbs)
+    while v1 == v2:
+        v2 = choice(nouns)
+    while v1 == v3 or v2 == v3:
+        n3 = choice(verbs)
 
+    # Pull three adjectives 
+    adj1 = choice(adjectives)
+    adj2 = choice(adjectives)
+    adj3 = choice(adjectives)
+    while adj1 == adj2:
+        adj2 = choice(nouns)
+    while adj1 == adj3 or adj2 == adj3:
+        adj3 = choice(adjectives)
 
-    print(f'The poem starts something like this: {n} {v} {adj}, {pre} {adv}')
+    #Pull one adverb
+    adv1 = choice(adverbs)
+    
+    #Pull two prepositions
+    pre1 = choice(prepositions)
+    pre2 = choice(prepositions)
+    while pre1 == pre2:
+        pre2 = choice(prepositions)
+
+    if 'aeiou'.find(adj1[0]) != -1:  # first letter is a vowel
+        article = "An"
+    else:
+        article = "A"
+
+    # add line to poem
+    poem = f'{article} {adj1} {n1}\n\n'  # title of poem
+    poem = poem + f'{article} {adj1} {n1} {v1} {pre1} the {adj2} {n2}\n'
+    poem = poem + f'{adv1} the {n2} {v2}\n'
+    poem = poem + f'the {n2} {v3} {pre2} a {adj3} {n3}'
+
+    return poem
+
+print(make_poem())
