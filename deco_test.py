938c2f15b5d2ef785a25c4c7472ea4b656ecbb7a
OdJacobsen <od.p.jacobsen@gmail.com>
2019-04-11 13:30:49 +0200

    Changes * Added more example decorators * Added a template decorator

--- deco_test.py
+++ deco_test.py
@@ -1,9 +1,78 @@
 # Testing decorators
-from decorators import my_decorator
+from decorators import *
+import math
+''
 
-
-@my_decorator
+@timer
 def say_whee():
     print('Whee!')
 
-say_whee()
+
+@timer
+def waste_some_time(num_times):
+    for _ in range(num_times):
+        print(sum([i**2 for i in range(10000)]))
+
+
+# waste_some_time(999)
+
+@debug
+def make_greeting(name, age=None):
+    if age is None:
+        return f'Howdy {name}!'
+    else:
+        return f'Whoa {name}! {age} already, you are growing up!'
+
+
+# make_greeting('Thomas', age=112)
+
+math.factorial = debug(math.factorial)
+
+
+def approximate_e(terms=18):
+    return sum(1 / math.factorial(n) for n in range(terms))
+
+
+# approximate_e(5)
+
+
+@slowdown
+def countdown(from_number):
+    if from_number < 1:
+        print('Liftoff!')
+    else:
+        print(from_number)
+        countdown(from_number - 1)
+
+
+# countdown(10)
+
+# Plugins
+import random
+PLUGINS = dict()
+
+
+def register(func):
+    """Register a function as a plug-in"""
+    PLUGINS[func.__name__] = func
+    return func
+
+
+@register
+def say_hello(name):
+    return f'Hello {name}'
+
+
+@register
+def be_awesome(name):
+    return f'Hey, {name}, you are awesome!'
+
+
+def randomly_greet(name):
+    greeter, greeter_func = random.choice(list(PLUGINS.items()))
+    print(f'Using {greeter!r}')
+    return greeter_func(name)
+
+
+print(PLUGINS)
+randomly_greet('Alice')
--- decorators.py
+++ decorators.py
@@ -1,7 +1,11 @@
 # Add decorators here
+# functools preserve the function identity
+import functools
+import time
 
 
 def my_decorator(func):
+    @functools.wraps(func)
     def wrapper():
         print("Something before")
         func()
@@ -9,6 +13,7 @@ def my_decorator(func):
     return wrapper
 
 
+# make this work somehow
 def check_vowels(func):
     def wrapper():
         if 'aiuoe'.find(word[0]) != -1:
@@ -18,3 +23,59 @@ def check_vowels(func):
         func()
         return wrapper
 
+
+# Decorator Template
+def decorator(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        # Do something before
+        value = func(*args, **kwargs)
+        # Do something after
+        return value
+    return wrapper_decorator
+
+
+# Time functions
+def timer(func):
+    """time runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        # Do something before
+        start_time = time.perf_counter() # 1
+        value = func(*args, **kwargs)
+        # Do something after
+        end_time = time.perf_counter() # 2
+        run_time = end_time - start_time
+        print(f'Finished {func.__name__!r} in {run_time:.4f} secs')
+        return value
+    return wrapper_timer
+
+
+def debug(func):
+    """
+    Print the function signature and return value
+    Showing arguments coming in and out of the function
+    """
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        # Do something before
+        args_repr = [repr(a) for a in args]  # 1
+        kwargs_repr = [f'{k} = {v!r}' for k, v in kwargs.items()]  # 2
+        signature = ', '.join(args_repr + kwargs_repr)  # 3
+        print(f'Calling {func.__name__}({signature})')
+        value = func(*args, **kwargs)
+        # Do something after
+        print(f'{func.__name__!r} returned {value!r}')  # 4
+        return value
+    return wrapper_debug
+
+
+def slowdown(func):
+    """Sleep 1 second before calling the function"""
+    @functools.wraps(func)
+    def wrapper_slow_down(*args, **kwargs):
+        # Do something before
+        time.sleep(1)
+        return func(*args, **kwargs)
+        # Do something after
+    return wrapper_slow_down
